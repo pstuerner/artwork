95f96b00619d722280a6207e339dbaa11f668360
Philipp Stuerner <philipp.stuerner@web.de>
2023-06-20 16:10:35 +0200

    fix twitter bot bugs, take images from db if already exist, cut text to 240 characters, shorten url

--- artwork/__main__.py
+++ artwork/__main__.py
@@ -26,63 +26,76 @@ def generate(
     dt_str = date.strftime("%Y%m%d")
     dt_start = dt.strptime(f"{date.year} {date.isocalendar().week} 1", "%Y %W %w")
     campaign = db.timetable.find_one({"date":dt_start})
-
-    essay = generate_essay(campaign["prompt"]["essay"].format(name=campaign["data"][dt_str]))
-
-    cookies = db.config.find_one({"name":"lexica"})["cookies"]
-    urls = Lexica(
-        query=campaign["prompt"]["artwork"].format(name=campaign["data"][dt_str]),
-        width=campaign["width"],
-        height=campaign["height"],
-        model="lexica-aperture-v3",
-        cookie=cookies[0] if now.day <= 15 else cookies[1]
-    ).generate()
-
-    images = {
-        1: [],
-        0.5: [],
-        0.25: [],
-        0.125: [],
-        0.02: []
-    }
+    name = campaign["data"][dt_str]
+    artwork_exists = db.artwork.count_documents({"date":date}) > 0
     image_paths = []
-    
-    for i, url in enumerate(urls):
-        im = Image.open(requests.get(url, stream=True).raw)
-        im.save(path_data / "temp" / f"{i}.jpg")
-        image_paths.append(path_data / "temp" / f"{i}.jpg")
 
-        for res in [1, 0.5, 0.25, 0.125, 0.02]:
-            image_bytes = io.BytesIO()
-            im_resize = im.resize([int(res * s) for s in im.size]) if res != 1 else im
-            im_resize.save(image_bytes, format='JPEG')
+    if not artwork_exists:
+        essay = generate_essay(campaign["prompt"]["essay"].format(name=name))
+        
+        cookies = db.config.find_one({"name":"lexica"})["cookies"]
+        urls = Lexica(
+            query=campaign["prompt"]["artwork"].format(name=name),
+            width=campaign["width"],
+            height=campaign["height"],
+            model="lexica-aperture-v3",
+            cookie=cookies[0] if now.day <= 15 else cookies[1]
+        ).generate()
 
-            images[res].append(image_bytes.getvalue())
-
-    db.artwork.insert_one(
-        {
-            "date": date,
-            "topic": campaign["topic"],
-            "name": campaign["data"][dt_str],
-            "essay": essay,
-            "prompt": campaign["prompt"]["artwork"].format(name=campaign["data"][dt_str]),
-            "images_100": images[1],
-            "images_050": images[0.5],
-            "images_025": images[0.25],
-            "images_0125": images[0.125],
-            "images_002": images[0.02],
+        images = {
+            1: [],
+            0.5: [],
+            0.25: [],
+            0.125: [],
+            0.02: []
         }
-    )
+        
+        for i, url in enumerate(urls):
+            im = Image.open(requests.get(url, stream=True).raw)
+            im.save(path_data / "temp" / f"{i}.jpg")
+            image_paths.append(path_data / "temp" / f"{i}.jpg")
+
+            for res in [1, 0.5, 0.25, 0.125, 0.02]:
+                image_bytes = io.BytesIO()
+                im_resize = im.resize([int(res * s) for s in im.size]) if res != 1 else im
+                im_resize.save(image_bytes, format='JPEG')
+
+                images[res].append(image_bytes.getvalue())
+
+        db.artwork.insert_one(
+            {
+                "date": date,
+                "topic": campaign["topic"],
+                "name": name,
+                "essay": essay,
+                "prompt": campaign["prompt"]["artwork"].format(name=name),
+                "images_100": images[1],
+                "images_050": images[0.5],
+                "images_025": images[0.25],
+                "images_0125": images[0.125],
+                "images_002": images[0.02],
+            }
+        )
+    else:
+        artwork = db.artwork.find_one({"date":date})
+        essay = artwork["essay"]
+        
+        for i, image_bytes in enumerate(artwork["images_100"]):
+            im = Image.open(io.BytesIO(image_bytes))
+            im.save(path_data / "temp" / f"{i}.jpg")
+            image_paths.append(path_data / "temp" / f"{i}.jpg")
 
     media_ids = []
     for image_path in image_paths:
         media = clientV1.media_upload(image_path)
         media_ids.append(media.media_id_string)
     
-    cutoff = essay[int(len(essay)*.1):].find(". ") + int(len(essay)*.1)
-    text = essay[:cutoff + 1].replace("\n", "") + f"\n👀 https://philippstuerner.com/posts/everydays/{date.year}/{date.month}/{date.day}/day"
+    text_url = f"\n👀🔗https://philippstuerner.com/e/{date.year}/{date.month}/{date.day}"
+    available_length = 240 - len(text_url)
+    text_body = essay.replace(name,f"#{name.replace(' ','')}")[:available_length-3]+"..."
+    
     clientV2.create_tweet(
-        text = text[:240],
+        text = text_body + text_url,
         media_ids = media_ids
     )
 
